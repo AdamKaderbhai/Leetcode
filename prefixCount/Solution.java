46822c7508123134a3789f8374e11b0ddc983bf3
AdamKaderbhai <adamkaderbhai36@gmail.com>
2025-01-08 19:57:55 -0600

    Counting Words With a Given Prefix
    You are given an array of strings words and a string pref.
    
    Return the number of strings in words that contain pref as a prefix.
    
    A prefix of a string s is any leading contiguous substring of s.
    
    Example 1:
    
    Input: words = ["pay","attention","practice","attend"], pref = "at"
    Output: 2
    Explanation: The 2 strings that contain "at" as a prefix are: "attention" and "attend".
    Example 2:
    
    Input: words = ["leetcode","win","loops","success"], pref = "code"
    Output: 0
    Explanation: There are no strings that contain "code" as a prefix.
    
    Constraints:
    
    1 <= words.length <= 100
    1 <= words[i].length, pref.length <= 100
    words[i] and pref consist of lowercase English letters.

---
+++ prefixCount/Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int prefixCount(String[] words, String prefix) {
+        int count = 0;
+        for (String word : words) {
+            if (word.startsWith(prefix)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
