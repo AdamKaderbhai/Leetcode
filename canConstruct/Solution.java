c2f6e80cc6544e5d3322a960db6470653b201984
AdamKaderbhai <adamkaderbhai36@gmail.com>
2025-01-10 22:03:10 -0600

    Construct K Palindrome Strings
    Given a string s and an integer k, return true if you can use all the characters in s to construct k palindrome strings or false otherwise.
    
    Example 1:
    
    Input: s = "annabelle", k = 2
    Output: true
    Explanation: You can construct two palindromes using all characters in s.
    Some possible constructions "anna" + "elble", "anbna" + "elle", "anellena" + "b"
    Example 2:
    
    Input: s = "leetcode", k = 3
    Output: false
    Explanation: It is impossible to construct 3 palindromes using all the characters of s.
    Example 3:
    
    Input: s = "true", k = 4
    Output: true
    Explanation: The only possible solution is to put each character in a separate string.
    
    Constraints:
    
    1 <= s.length <= 105
    s consists of lowercase English letters.
    1 <= k <= 105

---
+++ canConstruct/Solution.java
@@ -0,0 +1,25 @@
+public class Solution {
+
+    public boolean canConstruct(String s, int k) {
+        if (s.length() < k) {
+            return false;
+
+        }
+
+        int[] charCount = new int[26];
+        for (char c : s.toCharArray()) {
+            charCount[c - 'a']++;
+        }
+
+        int oddCount = 0;
+        for (int count : charCount) {
+            if (count % 2 == 1) {
+                oddCount++;
+            }
+        }
+
+        return oddCount <= k;
+
+    }
+    
+}
