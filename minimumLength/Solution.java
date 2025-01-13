47cb3126e01e5b741f228c4ded5dbdc70390fbf6
AdamKaderbhai <adamkaderbhai36@gmail.com>
2025-01-12 23:58:27 -0600

    Minimum Length of String After Operations Attempted
    You are given a string s.
    
    You can perform the following process on s any number of times:
    
    Choose an index i in the string such that there is at least one character to the left of index i that is equal to s[i], and at least one character to the right that is also equal to s[i].
    Delete the closest character to the left of index i that is equal to s[i].
    Delete the closest character to the right of index i that is equal to s[i].
    Return the minimum length of the final string s that you can achieve.
    
    Example 1:
    
    Input: s = "abaacbcbb"
    
    Output: 5
    
    Explanation:
    We do the following operations:
    
    Choose index 2, then remove the characters at indices 0 and 3. The resulting string is s = "bacbcbb".
    Choose index 3, then remove the characters at indices 0 and 5. The resulting string is s = "acbcb".
    Example 2:
    
    Input: s = "aa"
    
    Output: 2
    
    Explanation:
    We cannot perform any operations, so we return the length of the original string.

--- minimumLength/Solution.java
+++ minimumLength/Solution.java
@@ -1,20 +1,32 @@
 public class Solution {
     public int minimumLength(String s) {
-        int left = 0;
-        int right = s.length() - 1;
+        StringBuilder sb = new StringBuilder(s);
+        boolean removed = true;
 
-        while (left < right && s.charAt(left) == s.charAt(right)) {
-            char currentChar = s.charAt(left);
-            // Skip over matching characters on the left
-            while (left <= right && s.charAt(left) == currentChar) {
-                left++;
-            }
-            // Skip over matching characters on the right
-            while (left <= right && s.charAt(right) == currentChar) {
-                right--;
+        while (removed) {
+            removed = false;
+            for (int i = 1; i < sb.length() - 1; i++) {
+                char current = sb.charAt(i);
+                int left = i - 1;
+                int right = i + 1;
+
+                while (left >= 0 && sb.charAt(left) != current) {
+                    left--;
+                }
+                while (right < sb.length() && sb.charAt(right) != current) {
+                    right++;
+                }
+
+                if (left >= 0 && right < sb.length() && sb.charAt(left) == current && sb.charAt(right) == current) {
+                    sb.deleteCharAt(right);
+                    sb.deleteCharAt(left);
+                    removed = true;
+                    break;
+                }
             }
         }
 
-        return right - left + 1;
+        return sb.length();
     }
+
 }
